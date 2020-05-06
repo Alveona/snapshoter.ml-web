3a167efbea249797ca0c91b0727c270da7f1f170
Alveona <pomavau@yandex.ru>
2020-05-06 03:11:24 +0300

    Add img tag and correct snapshot fetching and showing

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,9 @@ export default class Home extends React.Component {
             copy: null,
             buttonText: "Screenshot",
             error: null,
-            clicks: null
+            clicks: null,
+            imgVisible: false,
+            imgUrl: null
         };
 
         // StatsController.mostClicked().then((response) => {
@@ -43,39 +45,11 @@ export default class Home extends React.Component {
                 </div>
 
                 {/* <h2>• Mostly clicked shorted links</h2> */}
-                {/* <div className="col-sm-12 col-md-10 offset-md-1">
-                    <table className="table table-borderless text-left table-responsive">
-                        <thead>
-                        <tr>
-                            <th scope="col">Source</th>
-                            <th scope="col">Links</th>
-                            <th scope="col">Clicks</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.state.clicks && this.state.clicks.map((c, i) => {
-                                if (i < 10) {
-                                    return (<tr key={i}>
-                                        <td className="source">{c.url}</td>
-                                        <td>
-                                            <a target="_blank" className="link" href={"https://devro.club/" + c.hash}>https://devro.club/{c.hash}</a>
-                                            &nbsp;&nbsp;
-                                            <svg onClick={() => {Home.copyURL(c.hash)}} width="14px" aria-hidden="true" focusable="false" data-prefix="far" data-icon="copy"
-                                                 className="svg-inline--fa fa-copy fa-w-14" role="img"
-                                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                                <path fill="currentColor"
-                                                      d="M433.941 65.941l-51.882-51.882A48 48 0 0 0 348.118 0H176c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h224c26.51 0 48-21.49 48-48v-48h80c26.51 0 48-21.49 48-48V99.882a48 48 0 0 0-14.059-33.941zM266 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h74v224c0 26.51 21.49 48 48 48h96v42a6 6 0 0 1-6 6zm128-96H182a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h106v88c0 13.255 10.745 24 24 24h88v202a6 6 0 0 1-6 6zm6-256h-64V48h9.632c1.591 0 3.117.632 4.243 1.757l48.368 48.368a6 6 0 0 1 1.757 4.243V112z"></path>
-                                            </svg>
-                                        </td>
-                                        <td>{c.count}</td>
-                                    </tr>);
-                                }
-                            }
-                        )}
-                        </tbody>
-                    </table>
-                    <a className="stat" href="/stats">View more statistics →</a>
-                </div> */}
+                {console.log(this.state.imgVisible)}
+                {this.state.imgVisible ?
+                    <div className="result-image-div">
+                        <img className="result-image" src={this.state.imgUrl}></img>
+                    </div> : null}
             </div>
         );
     }
@@ -117,7 +91,10 @@ export default class Home extends React.Component {
                 this.setState({ error: "URL entered is invalid." });
             } else {
                 UrlController.shortener(this.state.url).then((response) => {
-                    this.setState({ url: response.data });
+                    console.log(response.data)
+                    this.setState({ url: response.data.snapshot_url });
+                    this.setState({ imgVisible: true });
+                    this.setState({ imgUrl: response.data.snapshot_url });
                     this.setState({ copy: "copy" });
                     this.setState({ buttonText: "Copy to Clipboard" });
                 });
@@ -129,6 +106,7 @@ export default class Home extends React.Component {
         this.setState({ copy: null });
         this.setState({ url: '' });
         this.setState({ buttonText: "Screenshot" });
+        this.setState({ imgVisible: false });
     };
 
     validURL = (str) => {
--- src/controllers/UrlController.js
+++ src/controllers/UrlController.js
@@ -3,11 +3,10 @@ import axios from 'axios';
 class UrlController {
     shortener = async (url) => {
         return await axios.post(
-            'https://devro.club/api/shortener',
+            'http://127.0.0.1:8000/api/snapshot',
             JSON.stringify({ url: url })
         );
     };
-
 }
 
 export default (new UrlController());
