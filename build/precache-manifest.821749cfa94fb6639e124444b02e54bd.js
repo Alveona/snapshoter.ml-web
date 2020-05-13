018a7419949ff7238f463bfba92906b9d0409803
Alveona <pomavau@yandex.ru>
2020-05-13 22:56:17 +0300

    Add production build

---
+++ build/precache-manifest.821749cfa94fb6639e124444b02e54bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10216e0290101037babf83b974257cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebfbc0777f818929e043",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "feff8ecccf563c44bdb5",
+    "url": "/static/css/main.4035e4c9.chunk.css"
+  },
+  {
+    "revision": "ebfbc0777f818929e043",
+    "url": "/static/js/2.cf39ada1.chunk.js"
+  },
+  {
+    "revision": "feff8ecccf563c44bdb5",
+    "url": "/static/js/main.9d466bae.chunk.js"
+  },
+  {
+    "revision": "1439b78a30cdfec57dc4",
+    "url": "/static/js/runtime~main.88c4c3af.js"
+  },
+  {
+    "revision": "921935612e38ccbd1331cfa8160165fd",
+    "url": "/static/media/logo.92193561.png"
+  }
+]);
